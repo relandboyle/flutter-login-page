84b11f4fa95945986fba437c832327c20b7040c5
Reland <reland.boyle@gmail.com>
2024-01-24 21:38:45 -0500

    Login page layout

---
+++ lib/components/my_square_tile.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MySquareTile extends StatelessWidget {
+  final String imagePath;
+  const MySquareTile({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white),
+        borderRadius: BorderRadius.circular(16.0),
+        color: Colors.grey[200]
+      ),
+      child: Image.asset(
+        imagePath,
+        height: 40
+      )
+    );
+  }
+}
