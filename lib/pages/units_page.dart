b69ce39cc20e115cc4668e92574897b6b491aa4b
Reland <reland.boyle@gmail.com>
2024-02-09 09:00:52 -0500

    FAB padding

--- lib/pages/units_page.dart
+++ lib/pages/units_page.dart
@@ -1,16 +1,111 @@
 import 'package:flutter/material.dart';
 
+import '../components/my_textfield.dart';
+
 class UnitsPage extends StatelessWidget {
   const UnitsPage({super.key});
 
-  @override
+ @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('UNITS'),
-        ],
+    TextEditingController unitNumber = TextEditingController();
+    TextEditingController tenantName = TextEditingController();
+    TextEditingController buildingId = TextEditingController();
+
+    return Center(
+      child: SizedBox(
+        width: 500,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('UNITS'),
+              const SizedBox(height: 40),
+              MyTextField(
+                controller: unitNumber,
+                hintText: 'Street Number',
+                obscureText: false,
+              ),
+              const SizedBox(height: 20),
+              MyTextField(
+                controller: tenantName,
+                hintText: 'Street Name',
+                obscureText: false,
+              ),
+              const SizedBox(height: 20),
+              MyTextField(
+                controller: buildingId,
+                hintText: 'City',
+                obscureText: false,
+              ),
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                      ),
+                      elevation: MaterialStateProperty.all(10),
+                      backgroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.primaryContainer),
+                      foregroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.secondaryContainer),
+                      padding: MaterialStateProperty.all(
+                        const EdgeInsets.all(15),
+                      ),
+                    ),
+                    child: const Text('Find Unit'),
+                  ),
+                  const SizedBox(width: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                      ),
+                      elevation: MaterialStateProperty.all(10),
+                      backgroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.primaryContainer),
+                      foregroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.secondaryContainer),
+                      padding: MaterialStateProperty.all(
+                        const EdgeInsets.all(15),
+                      ),
+                    ),
+                    child: const Text('Update Unit'),
+                  ),
+                  const SizedBox(width: 20),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                      ),
+                      elevation: MaterialStateProperty.all(10),
+                      backgroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.primaryContainer),
+                      foregroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.secondaryContainer),
+                      padding: MaterialStateProperty.all(
+                        const EdgeInsets.all(15),
+                      ),
+                    ),
+                    child: const Text('Delete Unit'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 50),
+            ],
+          ),
+        ),
       ),
     );
   }
