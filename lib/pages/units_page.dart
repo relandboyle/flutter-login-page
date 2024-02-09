dc85bdce599bf4d1e6e7c7484c8ec57a0540a896
Reland <reland.boyle@gmail.com>
2024-02-09 11:14:39 -0500

    Padding top and bottom on all pages

--- lib/pages/units_page.dart
+++ lib/pages/units_page.dart
@@ -15,95 +15,99 @@ class UnitsPage extends StatelessWidget {
       child: SizedBox(
         width: 500,
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('UNITS'),
-              const SizedBox(height: 40),
-              MyTextField(
-                controller: unitNumber,
-                hintText: 'Street Number',
-                obscureText: false,
-              ),
-              const SizedBox(height: 20),
-              MyTextField(
-                controller: tenantName,
-                hintText: 'Street Name',
-                obscureText: false,
-              ),
-              const SizedBox(height: 20),
-              MyTextField(
-                controller: buildingId,
-                hintText: 'City',
-                obscureText: false,
-              ),
-              const SizedBox(height: 40),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all(
-                        const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(5),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20.0),
+                const Text('UNITS'),
+                const SizedBox(height: 40),
+                MyTextField(
+                  controller: unitNumber,
+                  hintText: 'Street Number',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 20),
+                MyTextField(
+                  controller: tenantName,
+                  hintText: 'Street Name',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 20),
+                MyTextField(
+                  controller: buildingId,
+                  hintText: 'City',
+                  obscureText: false,
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5),
+                            ),
                           ),
                         ),
-                      ),
-                      elevation: MaterialStateProperty.all(10),
-                      backgroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.primaryContainer),
-                      foregroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.secondaryContainer),
-                      padding: MaterialStateProperty.all(
-                        const EdgeInsets.all(15),
-                      ),
-                    ),
-                    child: const Text('Find Unit'),
-                  ),
-                  const SizedBox(width: 20),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all(
-                        const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(5),
-                          ),
+                        elevation: MaterialStateProperty.all(10),
+                        backgroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.primaryContainer),
+                        foregroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.secondaryContainer),
+                        padding: MaterialStateProperty.all(
+                          const EdgeInsets.all(15),
                         ),
                       ),
-                      elevation: MaterialStateProperty.all(10),
-                      backgroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.primaryContainer),
-                      foregroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.secondaryContainer),
-                      padding: MaterialStateProperty.all(
-                        const EdgeInsets.all(15),
-                      ),
+                      child: const Text('Find Unit'),
                     ),
-                    child: const Text('Update Unit'),
-                  ),
-                  const SizedBox(width: 20),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all(
-                        const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(5),
+                    const SizedBox(width: 20),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5),
+                            ),
                           ),
                         ),
+                        elevation: MaterialStateProperty.all(10),
+                        backgroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.primaryContainer),
+                        foregroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.secondaryContainer),
+                        padding: MaterialStateProperty.all(
+                          const EdgeInsets.all(15),
+                        ),
                       ),
-                      elevation: MaterialStateProperty.all(10),
-                      backgroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.primaryContainer),
-                      foregroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.secondaryContainer),
-                      padding: MaterialStateProperty.all(
-                        const EdgeInsets.all(15),
-                      ),
+                      child: const Text('Update Unit'),
                     ),
-                    child: const Text('Delete Unit'),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 50),
-            ],
+                    const SizedBox(width: 20),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5),
+                            ),
+                          ),
+                        ),
+                        elevation: MaterialStateProperty.all(10),
+                        backgroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.primaryContainer),
+                        foregroundColor: MaterialStateProperty.all(Theme.of(context).colorScheme.secondaryContainer),
+                        padding: MaterialStateProperty.all(
+                          const EdgeInsets.all(15),
+                        ),
+                      ),
+                      child: const Text('Delete Unit'),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 50),
+              ],
+            ),
           ),
         ),
       ),
